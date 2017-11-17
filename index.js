fb66c8dc2c08e723baea2fcc9f2e60b9a64864e7
Dean Stigall <stigallrealestate@gmail.com>
2017-11-17 00:22:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 // Write your code in this file!
-let scuberGreetingForFeet = 199;
-let ternaryCheckCity
-let switchOnCharmFromTip
 function scuberGreetingForFeet(distance){
   if (distance <= 400){
     console.log('This one is on me!');
