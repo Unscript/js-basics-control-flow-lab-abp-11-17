30dd0833966d4bede4de4d122c2952c2cc09867f
Dean Stigall <stigallrealestate@gmail.com>
2017-11-17 00:20:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 let scuberGreetingForFeet = 199;
 let ternaryCheckCity
 let switchOnCharmFromTip
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    console.log('This one is on me!');
+  }
+}
